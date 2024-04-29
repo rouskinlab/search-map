8d0df240648d5c1cfb93c9a09a78b28fecd883e9
Matty Allan <31744230+matthewfallan@users.noreply.github.com>
2024-04-29 15:15:07 -0400

    Change CRAM to BAM in consensus.sh for compatibility with SEISMIC-RNA

--- Compute/tgev-virus/consensus.sh
+++ Compute/tgev-virus/consensus.sh
@@ -2,5 +2,5 @@
 
 seismic align --cut-nextseq --no-fastqc -o consensus -x fq/virus/tgev-ut_R1.fq.gz -x fq/virus/tgev-ut_R2.fq.gz ref/tgev_refseq.fa
 
-samtools sort consensus/tgev-ut/align/NC_038861.1.cram | samtools consensus -a -f fasta -o ref/tgev_cons.fa -
+samtools sort consensus/tgev-ut/align/NC_038861.1.bam | samtools consensus -a -f fasta -o ref/tgev_cons.fa -
 
